15db20095ba605379de667bc0748bc7179a0078a
ykokubo <ykokubo@ykokubo-virtualbox.otr-jp.com>
2012-06-27 14:12:19 +0900

    show issues on master backlog

--- app/controllers/rb_master_backlogs_controller.rb
+++ app/controllers/rb_master_backlogs_controller.rb
@@ -25,6 +25,20 @@ class RbMasterBacklogsController < RbApplicationController
     @sprint_backlogs = sprints.map{ |s| { :sprint => s, :stories => sprints_backlog_storie_of[s.id] } }
     @c_sprint_backlogs = c_sprints.map{|s| { :sprint => s, :stories => sprints_backlog_storie_of[s.id] } }
 
+
+    @issue_trackers = @project.trackers.all.delete_if {|t| t.id == RbTask.tracker or RbStory.trackers.include?(t.id) }
+    @issues = Array.new
+    @issues_backlog = Array.new
+    @issue_trackers.each do |tracker|
+      is = RbStory.find(
+                          :all, 
+                          :conditions => ["project_id=? AND tracker_id = (?)", @project, tracker],
+                          :order => "position ASC"
+                          ).delete_if {|i| i.closed?}
+      @issues << is
+      @issues_backlog << {:sprint => nil, :tracker => tracker, :stories => is}
+    end
+
     respond_to do |format|
       format.html { render :layout => "rb"}
     end
