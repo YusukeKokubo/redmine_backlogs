738e5689d4d51d699a32a718c686ef2e363f1d24
ykokubo <ykokubo@ykokubo-virtualbox.otr-jp.com>
2012-06-27 16:53:07 +0900

    patch for issue

--- lib/backlogs_issue_patch_ex.rb
+++ lib/backlogs_issue_patch_ex.rb
@@ -7,68 +7,61 @@ module Backlogs
   def self.included(base)
     base.send(:include, InstanceMethods)
     base.class_eval do
-      alias_method_chain :before_save, :require_tasks_done_when_story_done
-      alias_method_chain :before_save, :require_estimated_hours_when_status_update
-      alias_method_chain :before_save, :require_assignee_when_status_in_progress
-      alias_method_chain :before_save, :note_required_on_close
+      before_save :require_tasks_done_when_story_done
+      before_save :require_estimated_hours_when_status_update
+      before_save :require_assignee_when_status_in_progress
+      before_save :note_required_on_close
     end
   end
   
   module InstanceMethods
-    def before_save_with_require_tasks_done_when_story_done
-      valid_save = before_save_without_require_tasks_done_when_story_done
+    def require_tasks_done_when_story_done
+      return unless project.module_enabled?('backlogs')
+      return unless Setting.plugin_redmine_backlogs[:story_trackers].map{|t|t.to_i}.include?(tracker_id)
+      return unless status_id_changed?
+      return unless IssueStatus.find(status_id).is_closed?
+      return if children.select {|task| !task.closed?}.empty?
 
-      return valid_save unless project.module_enabled?('backlogs')
-      return valid_save unless Setting.plugin_redmine_backlogs[:story_trackers].map{|t|t.to_i}.include?(tracker_id)
-      return valid_save unless status_id_changed?
-      return valid_save unless IssueStatus.find(status_id).is_closed?
-      return valid_save if children.select {|task| !task.closed?}.empty?
-
-      errors.add_to_base("タスクをすべて終了させてからストーリを終了させてね")
+      errors[:base] << :tasks_must_be_done_when_story_done
       false
     end
     
-    def before_save_with_require_estimated_hours_when_status_update
-      valid_save = before_save_without_require_estimated_hours_when_status_update
+    def require_estimated_hours_when_status_update
+      return unless project.module_enabled?('backlogs')
+      return unless tracker_id == Setting.plugin_redmine_backlogs[:task_tracker].to_i
+      return if new_record?
+      return unless status_id_changed?
+      return if IssueStatus.find(status_id).is_closed? and status.backlog == :failure
 
-      return valid_save unless project.module_enabled?('backlogs')
-      return valid_save unless tracker_id == Setting.plugin_redmine_backlogs[:task_tracker].to_i
-      return valid_save if new_record?
-      return valid_save unless status_id_changed?
-      return valid_save if IssueStatus.find(status_id).is_closed? and status.backlog == :failure
-
-      return valid_save if estimated_hours and estimated_hours > 0.0
+      return if estimated_hours and estimated_hours > 0.0
       
-      errors.add_to_base("旦那…。予定時間が入っていませんぜ？")
+      errors[:base] << :estimated_hours_is_empty
       false
     end
 
-    def before_save_with_require_assignee_when_status_in_progress
-      valid_save = before_save_without_require_assignee_when_status_in_progress
+    def require_assignee_when_status_in_progress
+      return unless project.module_enabled?('backlogs')
+      return unless tracker_id == Setting.plugin_redmine_backlogs[:task_tracker].to_i
+      return if new_record?
+      return unless status_id_changed?
+      return if IssueStatus.find(status_id).is_closed? and status.backlog == :failure
 
-      return valid_save unless project.module_enabled?('backlogs')
-      return valid_save unless tracker_id == Setting.plugin_redmine_backlogs[:task_tracker].to_i
-      return valid_save if new_record?
-      return valid_save unless status_id_changed?
-      return valid_save if IssueStatus.find(status_id).is_closed? and status.backlog == :failure
-
-      return valid_save if assigned_to
+      return if assigned_to
       
-      errors.add_to_base("先生！担当者がいません！")
+      errors[:base] << :assignee_is_empty
       false
     end
 
     # original is https://github.com/ajwalters/redmine_require_closing_note/tree
-    def before_save_with_note_required_on_close
-      valid_save = before_save_without_note_required_on_close
+    def note_required_on_close
       if require_note?
         # New records do _NOT_ have a notes field
         if @current_journal.notes.blank?
-          errors.add_to_base("後から思い出せるように経緯を書いてください。却下するなら却下した理由がありますよね。")
-          valid_save = false
+          errors[:base] << :require_note
+          return false
         end
       end
-      valid_save #= valid_save ? before_save_without_note_required_on_close : false
+      true #= valid_save ? before_save_without_note_required_on_close : false
     end
     
     private
